41b06ccc12cffcc4238c56e692a2e4189a2a22b1
ChetanPatil34 <cp9210639@gmail.com>
2024-02-09 19:34:43 +0530

    [ADDED] : Evenodd Code

---
+++ program1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	
+	printf("enter the any number");
+	scanf("%d",&num);
+	
+	if(num%2==0)
+	{
+		printf("\neven");
+	}
+	else
+	{
+		printf("\nodd");
+	}
+	
+	return 0;
+}
+
+
+
