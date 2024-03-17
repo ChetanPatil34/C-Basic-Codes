de1378c5e453c36439aa10f9fbef63fcbd527c1d
ChetanPatil34 <cp9210639@gmail.com>
2024-03-17 23:46:21 +0530

    [ADDED]:CHETAN Swaping code

---
+++ swap.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void swap(int x,int y)
+{
+	int temp;
+	temp=x;
+	x=y;
+	y=temp;
+}
+int main()
+{
+   int x=10;
+   int y=11;
+   
+   printf("value before swap:x=%d,y=%d",x,y);
+   swap(x,y);
+   printf("value after swap:x=%d,y=%d",x,y);
+}
+
+
