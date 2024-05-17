80bfb727467dbfa72999a3c1bd3d4b62ae9ff411
ChetanPatil34 <cp9210639@gmail.com>
2024-05-17 22:10:18 +0400

    [ADDED]:chetan C basic code

---
+++ shavstic.c
@@ -0,0 +1,19 @@
+//HIS CODE IS MADE A SWASTIC 
+#include<stdio.h>
+int main()
+{ 
+
+	printf("\t*********");
+	printf("\n*\t*");
+	printf("\n*\t*");
+	printf("\n*\t*");
+    printf("\n************");
+	printf("*****");
+	printf("\n\t*\t*");
+	printf("\n\t*\t*");
+	printf("\n\t*\t*");
+	printf("\t\n*********");
+	
+	
+	return 0;
+}
