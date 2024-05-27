6ce147ab04d380c662b6b03b5aea3d382630e9c4
ChetanPatil34 <cp9210639@gmail.com>
2024-05-27 23:43:18 +0400

    [ADDED]:chetan c program code two sum number

---
+++ textnew.c
@@ -0,0 +1,16 @@
+
+#include<iostream>
+int main()
+{
+	int a,b;
+	
+	cout<<"enter the number";
+	cin>>a;
+	
+	cout<<"enter the number";
+	cin>>b;
+	
+	cout<<"sum of two number is:"<<a+b;
+	
+	return 0;
+}
