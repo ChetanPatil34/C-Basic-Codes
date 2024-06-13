c77124773e53f16391dac95dd348ea26d93bc472
ChetanPatil34 <cp9210639@gmail.com>
2024-06-13 23:01:08 +0400

    [ADDED]:CHETAN Arithmetic Opertors code in C language

---
+++ arithmeticopertors.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a=7,b=5,c;
+	c=a+b;
+	printf("a+b=%d\n",c);
+	c=a-b;
+	printf("a-b=%d\n",c);
+	c=a*b;
+	printf("a*b=%d\n",c);
+	c=a/b;
+	printf("a/b=%d\n",c);
+	c=a%b;
+	printf("remainder when a is divided by b=%d\n",c);
+	return 0;
+}
