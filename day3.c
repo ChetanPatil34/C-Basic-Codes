a44f7ef676892448684bfac4332cce7ce8e1b9cc
ChetanPatil34 <cp9210639@gmail.com>
2024-06-14 22:42:48 +0400

    [ADDED]:CHETAN C language basic Code in if else statment

---
+++ day3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	
+	 if a>b;
+	{
+		printf("\nA is big");
+		scanf("%d",&a);
+		
+	}
+	else
+	{
+		printf("\nB is big");
+		scanf("%d",&b);
+	}
+	return 0;
+}
